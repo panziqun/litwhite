6275dc2b50af1e41d750151f5366ae2478864484
hp66722667 <66722667@qq.com>
2017-10-21 19:47:20 +0800

    添加plateList.html plateListData.html两个页面

--- application/admin/controller/Plate.php
+++ application/admin/controller/Plate.php
@@ -1,12 +1,38 @@
 <?php  
 namespace app\admin\controller;
+use app\admin\model\Plate;
 
 class Plate extends Auth{
-	function plateSet()
+	protected $plate;
+	public function _initialize()
+	{
+		$this->plate = new Plate();
+	}
+	public function plateList()
+	{
+		
+		//$plateList = $this->plate->getPlateList();
+		$plateListSelect = $this->plate->getPlateListSelect();
+		$this->assign([
+			'plateListSelect'=>$plateListSelect
+	]);
+		return $this->fetch();
+	}
+	public function plateListData()
+	{
+		$id = $this->request->param('id');
+		$plateListData = $this->plate->getPlateList((int)$id);
+		$this->assign([
+			'plateListData'=>$plateListData
+		]);
+		return $this->fetch();
+		
+	}
+	public function plateSet()
 	{
 		return $this->fetch();
 	}
-	function plateHidden()
+	public function plateHidden()
 	{
 		return $this->fetch();
 	}
