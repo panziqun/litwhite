db8dbb58df913ae8eb6088aa30861f0d96494db7
hp66722667 <66722667@qq.com>
2017-10-23 21:35:14 +0800

    2017/10/23 plate模块功能全部实现，还有一个plateListHiddenData.html的多余文件，用于后续增加回收站的删选功能，plate模块暂时不再更新

--- application/admin/controller/Plate.php
+++ application/admin/controller/Plate.php
@@ -4,6 +4,7 @@ use app\admin\model\Plate;
 
 class Plate extends Auth{
 	protected $plate;
+	protected $is_login = ['*'];
 	/*
 	*获取plateList.html页面中select标签的信息显示
 	*
@@ -34,6 +35,18 @@ class Plate extends Auth{
 		return $this->fetch();
 	}
 	/*
+	*获取plateList.html页面中tbody的的信息显示
+	*
+	*/
+	public function plateListHiddenData()
+	{
+		$plateListData = $this->plate->getPlateListHidden();
+		$this->assign([
+			'plateListData'=>$plateListData
+		]);
+		return $this->fetch();
+	}
+	/*
 	*获取plateSet.html页面中的信息显示
 	*
 	*/
@@ -49,27 +62,73 @@ class Plate extends Auth{
 	{
 		$plateInfo = $this->request->param();
 		$result = $this->plate->addPlate($plateInfo);
-		return $result;
+		return json_encode($result,JSON_UNESCAPED_UNICODE);
 	}
-	public function plateModify()
+	public function plateAddSun()
 	{
+		$plateID = $this->request->param('plateId');
 		$plateListSelect = $this->plate->getPlateListSelect();
-		
+		$plateInfo = $this->plate->getPlateInfo($plateID);
 		$this->assign([
+			'plateInfo'=>$plateInfo,
 			'plateListSelect'=>$plateListSelect
 		]);
 		return $this->fetch();
 	}
+	public function plateModify()
+	{	
+		$plateID = $this->request->param('plateId');
+		$plateListSelect = $this->plate->getPlateListSelect();
+		$plateInfo = $this->plate->getPlateInfo($plateID);
+		$this->assign([
+			'plateInfo'=>$plateInfo,
+			'plateListSelect'=>$plateListSelect
+		]);
+		return $this->fetch();
+	}
+
+	public function plateModifyInfo()
+	{
+		$plateInfo = $this->request->param();
+		$result = $this->plate->plateModifyData($plateInfo);
+
+		return json_encode($result,JSON_UNESCAPED_UNICODE);
+	}
+	
+	public function plateModifyCheckTitle()
+	{
+		$info = $this->request->param();
+		if (empty($info['title'])) {
+			return json_encode(['code'=>200,'info'=>'标题不能为空'],JSON_UNESCAPED_UNICODE);
+		}
+		$result = $this->plate->plateCheckTitleExistById($info['title'],$info['id']);
+		return json_encode($result,JSON_UNESCAPED_UNICODE);
+	}
+
 	public function plateAddCheckTitle()
 	{
 		$plateTitle = $this->request->param('title');
+		if (empty($plateTitle)) {
+			return json_encode(['code'=>200,'info'=>'标题不能为空'],JSON_UNESCAPED_UNICODE);
+		}
 		$result = $this->plate->plateCheckTitleExist($plateTitle);
-		return $result;
+		return json_encode($result,JSON_UNESCAPED_UNICODE);
 	}
-
 	public function plateHidden()
 	{
+		$plateListData = $this->plate->getPlateListHidden();
+		$plateListSelect = $this->plate->getPlateListSelect();
+		$this->assign([
+			'plateListSelect'=>$plateListSelect,
+			'plateListData'=>$plateListData
+		]);
 		return $this->fetch();
+	}
+	public function plateListRecvData()
+	{
+		$id = $this->request->param('id');
+		$result = $this->plate->plateRecv($id);
+		return json_encode($result,JSON_UNESCAPED_UNICODE);
 	}
 }
 ?>
--- application/admin/model/Plate.php
+++ application/admin/model/Plate.php
@@ -6,6 +6,8 @@ use traits\model\SoftDelete;
 
 class Plate extends Model
 {
+	use SoftDelete;
+	protected $deleteTime = 'delete_time';
 	protected static $treeList = [];
 	public function getPlateList($id = 0)
 	{	
@@ -14,6 +16,12 @@ class Plate extends Model
 		$result = $this->getPlateTree($plateInfo,$id);
 		return $result;
 	}
+	public function getPlateListHidden()
+	{	
+		$plateInfo = $this->onlyTrashed()->select();
+		return $plateInfo;
+	}
+
 	public function getPlateListSelect()
 	{	
 		self::$treeList = [];
@@ -24,18 +32,41 @@ class Plate extends Model
 		}
 		return $result;
 	}
+	
 	/*
 	*在plate表中查找对应的字段信息是否存在
 	*
 	*/
-	public function plateCheckTitleExist($plateTitle)
+	public function plateCheckTitleExist($plateTitlel)
 	{
-		$result = $this->get(['plate_title'=>$plateTitle]);
-		if (isset($result)) {
+		$result = $this->get(['plate_title'=>$plateTitlel]);
+		if (!empty($result)) {
 			return ['code'=>200,'info'=>'标题已存在'];
 		} else {
 			return ['code'=>500,'info'=>'标题正确'];
 		}
+	}
+	/*
+	*在plate表中查找对应的字段信息是否存在
+	*
+	*/
+	public function plateCheckTitleExistById($plateTitle,$plateId = null)
+	{
+		$result = $this->where('plate_id','<>',(int)$plateId)->where('plate_title',$plateTitle)->select();	
+		if (!empty($result)) {
+			return ['code'=>200,'info'=>'标题已存在'];
+		} else {
+			return ['code'=>500,'info'=>'标题正确'];
+		}	
+	}
+	/*
+	*在plate表中根据plate_id查询对应的模块信息
+	*
+	*/
+	public function getPlateInfo($plateId)
+	{
+		$plateInfo = $this->get((int)$plateId)->toArray();	
+		return $plateInfo;
 	}
 	/*
 	*在plate表中添加新的模块信息
@@ -48,8 +79,33 @@ class Plate extends Model
 			'plate_order'=>(int)$plateInfo['order'],
 			'plate_fid'=>(int)$plateInfo['fid'],
 		]);
-		$this->save();
-		return $this->plate_id;
+		$result = $this->save();
+		if (((int)$plateInfo['hidden']==1)) {
+			$result = $this->destroy((int)$plateInfo['id']);
+		}
+		if ($result) {
+			return ['code'=>200,'info'=>'修改成功'];
+		} else {
+			return ['code'=>500,'info'=>'修改失败'];
+		}
+	}
+	public function plateModifyData($plateInfo)
+	{
+		$plateData = $this->get((int)$plateInfo['id']);
+		$plateData->data([
+			'plate_title'=>$plateInfo['title'],
+			'plate_order'=>(int)$plateInfo['order'],
+			'plate_fid'=>(int)$plateInfo['fid'],
+		]);
+		$result = $plateData->save();
+		if (((int)$plateInfo['hidden']==1)) {
+			$result = $this->destroy((int)$plateInfo['id']);
+		}
+		if ($result) {
+			return ['code'=>200,'info'=>'修改成功'];
+		} else {
+			return ['code'=>500,'info'=>'修改失败'];
+		}
 	}
 	/*
 	*获取plate表中所有的版块信息
@@ -70,5 +126,20 @@ class Plate extends Model
 			}
 		}
 		return self::$treeList;
+	}
+	/*
+	*在版块回收站中恢复plate信息
+	*
+	*/
+	public function plateRecv($plateId)
+	{
+		$plate = $this->onlyTrashed()->where('plate_id',(int)$plateId)->find();
+		$plate->delete_time = NULL;
+		$result = $plate->save();
+		if ($result) {
+			return ['code'=>200,'info'=>'修改成功'];
+		} else {
+			return ['code'=>500,'info'=>'修改失败'];
+		}
 	}
 }
