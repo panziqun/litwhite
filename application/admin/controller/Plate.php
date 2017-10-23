416c48fbf704b9a752ffa046031c00b8befeae6c
hp66722667 <66722667@qq.com>
2017-10-23 08:14:42 +0800

    2017/10/22 更新plate模块

--- application/admin/controller/Plate.php
+++ application/admin/controller/Plate.php
@@ -4,20 +4,26 @@ use app\admin\model\Plate;
 
 class Plate extends Auth{
 	protected $plate;
+	/*
+	*获取plateList.html页面中select标签的信息显示
+	*
+	*/
 	public function _initialize()
 	{
 		$this->plate = new Plate();
 	}
 	public function plateList()
 	{
-		
-		//$plateList = $this->plate->getPlateList();
 		$plateListSelect = $this->plate->getPlateListSelect();
 		$this->assign([
 			'plateListSelect'=>$plateListSelect
-	]);
+		]);
 		return $this->fetch();
 	}
+	/*
+	*获取plateList.html页面中tbody的的信息显示
+	*
+	*/
 	public function plateListData()
 	{
 		$id = $this->request->param('id');
@@ -26,12 +32,41 @@ class Plate extends Auth{
 			'plateListData'=>$plateListData
 		]);
 		return $this->fetch();
-		
 	}
+	/*
+	*获取plateSet.html页面中的信息显示
+	*
+	*/
 	public function plateSet()
 	{
+		$plateListSelect = $this->plate->getPlateListSelect();
+		$this->assign([
+			'plateListSelect'=>$plateListSelect
+		]);
 		return $this->fetch();
 	}
+	public function plateAdd()
+	{
+		$plateInfo = $this->request->param();
+		$result = $this->plate->addPlate($plateInfo);
+		return $result;
+	}
+	public function plateModify()
+	{
+		$plateListSelect = $this->plate->getPlateListSelect();
+		
+		$this->assign([
+			'plateListSelect'=>$plateListSelect
+		]);
+		return $this->fetch();
+	}
+	public function plateAddCheckTitle()
+	{
+		$plateTitle = $this->request->param('title');
+		$result = $this->plate->plateCheckTitleExist($plateTitle);
+		return $result;
+	}
+
 	public function plateHidden()
 	{
 		return $this->fetch();
--- application/admin/model/Plate.php
+++ application/admin/model/Plate.php
@@ -10,20 +10,54 @@ class Plate extends Model
 	public function getPlateList($id = 0)
 	{	
 		self::$treeList = [];
-		$arr = Db::table('lit_plate')->select();
-		
-		$result = $this->getPlateTree($arr,$id);
+		$plateInfo = $this->getAllPlateInfo();
+		$result = $this->getPlateTree($plateInfo,$id);
 		return $result;
 	}
 	public function getPlateListSelect()
 	{	
 		self::$treeList = [];
-		$arr = Db::table('lit_plate')->select();
-		$result = $this->getPlateTree($arr);
+		$plateInfo = $this->getAllPlateInfo();
+		$result = $this->getPlateTree($plateInfo);
 		foreach ($result as $key => $value) {
 			$result[$key]['plate_title'] = str_repeat(' &emsp; ', $value['count']) . '|--' . $value['plate_title'];
 		}
 		return $result;
+	}
+	/*
+	*在plate表中查找对应的字段信息是否存在
+	*
+	*/
+	public function plateCheckTitleExist($plateTitle)
+	{
+		$result = $this->get(['plate_title'=>$plateTitle]);
+		if (isset($result)) {
+			return ['code'=>200,'info'=>'标题已存在'];
+		} else {
+			return ['code'=>500,'info'=>'标题正确'];
+		}
+	}
+	/*
+	*在plate表中添加新的模块信息
+	*
+	*/
+	public function addPlate($plateInfo)
+	{
+		$this->data([
+			'plate_title'=>$plateInfo['title'],
+			'plate_order'=>(int)$plateInfo['order'],
+			'plate_fid'=>(int)$plateInfo['fid'],
+		]);
+		$this->save();
+		return $this->plate_id;
+	}
+	/*
+	*获取plate表中所有的版块信息
+	*
+	*/
+	protected function getAllPlateInfo()
+	{
+		return Db::table('lit_plate')->select();
 	}
 	private function getPlateTree($arr, $plate_fid = 0, $count = 0)
 	{
